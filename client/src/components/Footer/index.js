6ecf88b494a4f49a7f184f79dacbd2b15f091b7d
hmhtom <hmhtom@gmail.com>
2022-11-25 12:11:54 -0500

    footer styling

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="/">
+        NAS Centre
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+      }}>
+      <Container maxWidth="sm">
+        <Typography variant="body1">Develop with React</Typography>
+        <Copyright />
+      </Container>
+    </Box>
+  );
+}
