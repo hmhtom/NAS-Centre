48bf8d627d3ad569802e2d918b4cdde267135c06
NathanKryz <112029666+NathanKryz@users.noreply.github.com>
2022-11-28 13:47:49 -0500

    bugfixed and tested user/login routes, stable patch

--- server/server.js
+++ server/server.js
@@ -20,11 +20,18 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use("/", (req, res, next) => {
+  console.log(req.headers);
+  next();
+})
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+ });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
@@ -73,9 +80,7 @@ startApolloServer(typeDefs, resolvers);
 //   app.use(express.static(path.join(__dirname, "../client/build")));
 // }
 
-  app.get("*", (req, res) => {
-   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
+ 
 
 // const startApolloServer = async (typeDefs, resolvers) => {
 //     await server.start();
