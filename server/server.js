ea80cba1be5210c01542fc5fbec7134ae2ba8258
hmhtom <hmhtom@gmail.com>
2022-11-29 11:39:40 -0500

    deployment fix

--- server/server.js
+++ server/server.js
@@ -19,15 +19,16 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use("/", (req, res, next) => {
-//   console.log(req.headers);
-//   next();
-// })
-// if we're in production, serve client/build as static assets
+
 app.use("/images", express.static(path.join(__dirname, "../client/images")));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
